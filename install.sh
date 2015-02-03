fe311d7052251577b400da7b601a4fb577252332
rodrigozhou <rodrigo.zhou@gmail.com>
2015-02-02 23:25:57 -0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-git clone --recursive https://github.com/rodrigozhou/.vim.git $HOME/.vim
+git clone https://github.com/rodrigozhou/.vim.git $HOME/.vim
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 vim +PluginInstall
